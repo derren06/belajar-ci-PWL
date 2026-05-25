4b4b066d366f3efe78c60453d16ea38c33377c30
derren06 <derren.teknik@gmail.com>
2026-05-25 22:35:38 +0700

    read data dan validasi

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,21 @@
 
 namespace App\Controllers;
 
+use App\Models\ProductModel; 
+
 class Home extends BaseController
 {
+    protected $productModel;
+
+    function __construct()
+    {
+        $this->productModel = new ProductModel();
+    }
+
     public function index(): string
     {
-        return view('v_homepage');
+        return view('v_homepage', [
+            'products' => $this->productModel->findAll()
+        ]);
     }
 }
